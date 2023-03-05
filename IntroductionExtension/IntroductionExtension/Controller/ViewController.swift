dc291bab0b785a95d2bb8f5c23615f81744b4f9b
AykutIpek <aykutipek_2001@hotmail.com>
2023-03-05 12:20:04 +0300

    Label Position Added

--- IntroductionExtension/IntroductionExtension/Controller/ViewController.swift
+++ IntroductionExtension/IntroductionExtension/Controller/ViewController.swift
@@ -12,20 +12,13 @@ final class ViewController: UIViewController {
     // MARK: - Properties
     private let imageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "bear_first"))
+        imageView.contentMode = .scaleAspectFit
         return imageView
-    }()
-    private let descriptionTextView: UITextView = {
-        let textView = UITextView()
-        textView.text = "Join us today in our fun and games!"
-        textView.textColor = .red
-        textView.font = UIFont.systemFont(ofSize: 18)
-        textView.backgroundColor = .white
-        textView.textAlignment = .center
-        return textView
     }()
     private let labelFirst: UILabel = {
         let label = UILabel()
-        label.text = "Deneme"
+        label.text = "Join us today in our fun and games!"
+        label.font = UIFont.systemFont(ofSize: 18)
         label.textColor = .label
         return label
     }()
@@ -43,28 +36,21 @@ extension ViewController{
         layout()
     }
     private func style(){
-        view.backgroundColor = .systemBackground
+
     }
     private func layout(){
         view.addSubview(imageView)
-        view.addSubview(descriptionTextView)
         view.addSubview(labelFirst)
+        
         imageView.snp.makeConstraints { make in
             make.width.height.equalTo(200)
             make.centerX.equalToSuperview()
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
         }
-//
-//        descriptionTextView.snp.makeConstraints { make in
-//            make.top.equalTo(imageView.snp.bottom).offset(120).constraint.isActive = true
-//            make.left.equalToSuperview().constraint.isActive = true
-//            make.right.equalToSuperview().constraint.isActive = true
-//            make.centerX.equalToSuperview().constraint.isActive = true
-//        }
         
         labelFirst.snp.makeConstraints { make in
             make.top.equalTo(imageView.snp.bottom).offset(120)
-            make.left.equalToSuperview()
+            make.centerX.equalToSuperview()
         }
     }
 }
