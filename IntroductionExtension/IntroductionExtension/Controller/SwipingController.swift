033948b411758e67237508e7257a369eb7cf36b6
AykutIpek <aykutipek_2001@hotmail.com>
2023-03-07 15:40:44 +0300

    Complited Horizontal UI

--- IntroductionExtension/IntroductionExtension/Controller/SwipingController.swift
+++ IntroductionExtension/IntroductionExtension/Controller/SwipingController.swift
@@ -118,4 +118,17 @@ extension SwipingController: UICollectionViewDelegateFlowLayout{
         let x = targetContentOffset.pointee.x
         pageControl.currentPage = Int(x / view.frame.width)
     }
+    
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animate { _ in
+            self.collectionViewLayout.invalidateLayout()
+            if self.pageControl.currentPage == 0{
+                self.collectionView.contentOffset = .zero
+            }else{
+                let indexPath = IndexPath(item: self.pageControl.currentPage, section: 0)
+                self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            }
+        }
+    }
 }
